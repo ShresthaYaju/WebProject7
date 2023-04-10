ec26408267861a2de5bb0f0c288aad80fa776033
Yaju Shrestha <shresthayazu@gmail.com>
2023-04-10 13:55:05 -0500

    Add Create Crew page

--- project7/src/Components/CreateCrew.jsx
+++ project7/src/Components/CreateCrew.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function CreateCrew() {
+  const [crewmate, setCrewmate] = useState({ Name: "", Speed: "", Color: "" });
+
+  const handelChange = (e) => {
+    const { name, value } = e.target;
+    setCrewmate({ ...crewmate, [name]: value });
+  };
   return (
-    <div>CreateCrew</div>
-  )
+    <div className=" text-center mt-32 mb-auto h-1/2 ">
+      <h1 className="text-5xl font-bold">Create a New Crewmate</h1>
+      <img
+        src="https://www.pngmart.com/files/21/Among-Us-PNG-Isolated-Transparent.png"
+        alt=""
+        className="block ml-auto mr-auto w-1/2"
+        height={10}
+      />
+
+      <form action="">
+        <div className="flex gap-8 justify-center">
+          <div className=" bg-slate-700 p-9 rounded-xl">
+            <label className="block text-2xl mb-2"> Name</label>
+            <input
+              type="text"
+              value={crewmate.Name}
+              name="Name"
+              onChange={handelChange}
+              className="border-2 border-gray-400 rounded-xl"
+            />
+          </div>
+          
+            <div className=" bg-slate-700 p-9 rounded-xl">
+              <label className="block text-2xl mb-2"> Speed</label>
+              <input
+                type="text"
+                value={crewmate.Speed}
+                name="Speed"
+                onChange={handelChange}
+                className="border-2 border-gray-400 rounded-xl"
+              />
+            </div>
+        
+          <div className=" bg-slate-700 p-9 rounded-xl text-left">
+            <label className="block text-2xl mb-2"> Color</label>
+
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  onChange={handelChange}
+                  id="red"
+                  name="Color"
+                  value="red"
+                  className="mr-2"
+                />
+                Red
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  onChange={handelChange}
+                  id="blue"
+                  name="Color"
+                  value="blue"
+                  className="mr-2"
+                />
+                blue
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  onChange={handelChange}
+                  id="green"
+                  name="Color"
+                  value="green"
+                  className="mr-2"
+                />
+                Green
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  onChange={handelChange}
+                  id="pink"
+                  name="Color"
+                  value="pink"
+                  className="mr-2"
+                />
+                Pink
+              </li>
+            </ul>
+
+            {console.log(crewmate)}
+          </div>
+        </div>
+        <button className="p-4 bg-teal-600 rounded-xl mt-3 cursor-pointer hover:scale-105 transition-all">Create Crewmate</button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateCrew
+export default CreateCrew;
--- project7/src/Components/Home.jsx
+++ project7/src/Components/Home.jsx
@@ -4,26 +4,26 @@ import { Link, Outlet } from "react-router-dom";
 function Home() {
   return (
     <div className="flex flex-1 relative">
-      <div className="w-1/6 bg-slate-700 h-screen text-center ">
+      <div className="w-1/6 bg-slate-700 h-screen text-center pt-14">
         <div className="p-4 text-2xl font-semibold">
           <ul>
-            <li className="hover:scale-105 mb-3">
+            <li className="hover:scale-105 mb-5 border-2 border-teal-600 rounded-xl hover:bg-teal-600 duration-500">
               <Link to="/">Home</Link>
             </li>
-            <li className="hover:scale-105 mb-3">
+            <li className="hover:scale-105 mb-5 border-2 border-teal-600 rounded-xl  hover:bg-teal-600 duration-500">
               <Link to="/CreateCrew">Create a Crewmate!</Link>
             </li>
-            <li className="hover:scale-105 mb-3">
+            <li className="hover:scale-105 mb-5 border-2 border-teal-600 rounded-xl hover:bg-teal-600 duration-500">
               <Link to="/CrewmateGallery">Crewmate Gallery</Link>
             </li>
           </ul>
         </div>
-        <div className="flex flex-col-reverse h-3/4 items-center">
+        <div className="flex flex-col-reverse h-3/5 items-end justify-start">
           <Link to="/">
             <img
-              src="https://assets.stickpng.com/images/61d183263a856e0004c6334a.png"
+              src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/5d7564664b84a832897129b17d3d2ff2-1613763577/Attachment_1613763560/create-personalized-among-us-gif-and-png-for-you.gif"
               alt=""
-              width={150}
+              width={250}
             />
           </Link>
         </div>
